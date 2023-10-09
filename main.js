145ffdd3939ca96c2265ead67675e1ef566756a4
heritsilavo <lalaniaina.rabeson@gmail.com>
2023-10-09 15:53:55 +0300

    add  accordion

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 import './style.scss'
+import Swiper from 'swiper';
+import 'swiper/css';
+
+const humberger = document.querySelector('.humb')
+const navMobile = document.querySelector('.nav-mobile')
+const exitIcon = document.querySelector('.exit')
+
+humberger.addEventListener('click', () => {
+
+    navMobile.classList.toggle('active')
+})
+
+window.addEventListener('resize', (e) => {
+    if(e.target.innerWidth > 1600) {
+        navMobile.classList.remove('active')
+    }
+})
+exitIcon.addEventListener('click', () =>{
+    navMobile.classList.remove('active')
+
+})
