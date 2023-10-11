081f889807f43b549e317d4e9a95063a5b0220a7
heritsilavo <lalaniaina.rabeson@gmail.com>
2023-10-11 16:37:43 +0300

    add  swiper

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import './style.scss'
 import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';;
 
 const humberger = document.querySelector('.humb')
 const navMobile = document.querySelector('.nav-mobile')
@@ -20,3 +23,16 @@ exitIcon.addEventListener('click', () =>{
     navMobile.classList.remove('active')
 
 })
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
